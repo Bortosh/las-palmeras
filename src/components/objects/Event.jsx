f67d5c0cfdf8584b93afec646fd1c848042b7a0c
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2023-09-17 18:07:06 -0500

    adding new videos

--- src/components/objects/Event.jsx
+++ src/components/objects/Event.jsx
@@ -5,7 +5,10 @@ import { WrapperAbout, WrapperAboutDescription } from "../style";
 import Gallery from "./Gallery";
 import videoEvento1 from '../../assets/videos/videoEvento1.mp4'
 import videoEvento2 from '../../assets/videos/videoEvento2.mp4'
-import video6 from '../../assets/videos/video6.mp4'
+import videoEvento3 from '../../assets/videos/videoEvento3.mp4'
+import videoEvento4 from '../../assets/videos/videoEvento4.mp4'
+import videoEvento5 from '../../assets/videos/videoEvento5.mp4'
+import videoEvento6 from '../../assets/videos/videoEvento6.mp4'
 import evento1 from '../../assets/evento1.jpeg'
 import evento2 from '../../assets/evento2.jpeg'
 import evento3 from '../../assets/evento3.jpeg'
@@ -18,8 +21,12 @@ const Event = () => {
     const { language: { menu, textEvent, titleEvent }, effects: { getAssets } } = useContext(appContext)
 
     const videos = [
+        { poster: getAssets('imgVideoEvento4', 'jpeg'), src: videoEvento4 },
+        { poster: getAssets('imgVideoEvento5', 'jpeg'), src: videoEvento5 },
+        { poster: getAssets('imgVideoEvento2', 'jpeg'), src: videoEvento2 },
         { poster: getAssets('imgVideoEvento1', 'jpeg'), src: videoEvento1 },
-        { poster: getAssets('imgVideoEvento2', 'jpeg'), src: videoEvento2 }
+        { poster: getAssets('imgVideoEvento3', 'jpeg'), src: videoEvento3 },
+        { poster: getAssets('imgVideoEvento6', 'jpeg'), src: videoEvento6 },
     ]
 
     const imagenes = [evento1, evento2, evento3]
